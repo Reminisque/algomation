f91e0f1beaa4e5c3ac882d4712ad7f3e6e9fb39e
Reminisque <travisc2728@gmail.com>
2020-09-01 04:13:19 -0700

    Create general layout and change to module CSS

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import { Header, VisualPane, ControlPane } from './ui';
+import styles from './App.module.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className={styles.layoutContainer}>
+        <Header className={styles.header} brand="Algomation"></Header>
+        <VisualPane className={styles.visualPane}></VisualPane>
+        <ControlPane className={styles.controlPane}></ControlPane>
+      </div>
+    );
+  }
 }
 
 export default App;
