1218af97a38b1aec6e659630a8fd1a6a005f7321
Reminisque <travisc2728@gmail.com>
2020-09-17 23:23:00 -0700

    Create algorithms menu and toggle

--- src/ui/ControlPane.js
+++ src/ui/ControlPane.js
@@ -1,15 +1,34 @@
 import React from 'react';
+import { Button, BannerMenu, TwoBarBurgerIcon } from './components';
 import styles from './ControlPane.module.css';
 
 class ControlPane extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      openAlgoMenu: false
+    };
+
+    this.toggleOpenAlgoMenu = this.toggleOpenAlgoMenu.bind(this);
+  }
+
   render() {
     const { className } = this.props;
+    const menuOpened = this.state.openAlgoMenu ? styles.open : "";
 
     return (
       <div className={[styles.root, className].join(' ')}>
         {this.props.children}
+        <Button className={styles.algoMenuBtn} onClick={this.toggleOpenAlgoMenu}>
+          <TwoBarBurgerIcon></TwoBarBurgerIcon>
+        </Button>
+        <BannerMenu className={[styles.algoMenu, menuOpened].join(' ')}></BannerMenu>
       </div>
     )
+  }
+
+  toggleOpenAlgoMenu = () => {
+    this.setState({openAlgoMenu: !this.state.openAlgoMenu})
   }
 }
 
