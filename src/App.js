b0b508dd750466e67c21153540392065403d6a58
Reminisque <travisc2728@gmail.com>
2020-09-27 03:06:27 -0700

    Add placeholders for descriptions in control pane

--- src/App.js
+++ src/App.js
@@ -4,7 +4,41 @@ import { Header, VisualPane, ControlPane } from './ui';
 import styles from './App.module.css';
 
 class App extends React.Component {
+  algoMenu = [
+    {
+      title: 'Sorting',
+      algorithms: [
+        'Selection Sort',
+        'Insertion Sort',
+        'Merge Sort'
+      ]
+    },
+    {
+      title: 'Graph',
+      algorithms: [
+        'Djikstra\'s Algorithm'
+      ]
+    },
+    {
+      title: 'Linked List',
+      algorithms: [
+        'Search'
+      ] 
+    }
+  ];
+
+  pseudocode = [
+    'for loop',
+    ' if something if true:',
+    '   do stuff for this block',
+    ' else if something else',
+    '   do stuff for this other block',
+    'doing something else',
+    'return'
+  ];
+
   render() {
+
     return (
       <div className={styles.root}>
         <div className={styles.layoutContainer}>
@@ -14,11 +48,14 @@ class App extends React.Component {
             </div>
           </Header>
           <VisualPane className={styles.visualPane}></VisualPane>
-          <ControlPane className={styles.controlPane}></ControlPane>
+          <ControlPane className={styles.controlPane} menu={this.algoMenu} pseudocode={this.pseudocode} category={"Sorting"} algorithm={"Some really long sorting algorithm"}></ControlPane>
         </div>
       </div>
     );
   }
+
+  selectionSort
+  
 }
 
 export default App;
--- src/ui/ControlPane.js
+++ src/ui/ControlPane.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Menu, TwoBarBurgerIcon } from './components';
+import { Button, CodeBox, Menu, MenuItem, TwoBarBurgerIcon } from './components';
 import styles from './ControlPane.module.css';
 
 class ControlPane extends React.Component {
@@ -13,23 +13,40 @@ class ControlPane extends React.Component {
   }
 
   render() {
-    const { className } = this.props;
+    const { className, menu, category, algorithm, pseudocode} = this.props;
     const menuOpened = this.state.openAlgoMenu ? styles.open : '';
 
     return (
       <div className={[styles.root, className].join(' ')}>
-        {this.props.children}
         <Button className={styles.algoMenuBtn} onClick={this.toggleOpenAlgoMenu}>
           <TwoBarBurgerIcon></TwoBarBurgerIcon>
         </Button>
-        <Menu className={[styles.algoMenu, menuOpened].join(' ')}>
-        </Menu>
+        <div className={[styles.algoMenu, menuOpened].join(' ')}>
+          <div className={styles.algoMenuFlex}>
+            {menu.map((group) => 
+              <Menu className={styles.algoMenuGroup}>
+                {group.title ? 
+                  <MenuItem key={group.title} title>{group.title} </MenuItem>
+                  : null
+                }
+                {group.algorithms.map((algorithm) => 
+                  <MenuItem key={algorithm}>{algorithm}</MenuItem>
+                )}
+              </Menu>
+            )}
+          </div>
+        </div>
+        <div>
+          <div className={styles.category}>{category}</div>
+          <div className={styles.algorithm}>{algorithm}</div>
+        </div>
+        <CodeBox className={styles.pseudocode} code={pseudocode}></CodeBox>
       </div>
     )
   }
 
   toggleOpenAlgoMenu = () => {
-    this.setState({openAlgoMenu: !this.state.openAlgoMenu})
+    this.setState({openAlgoMenu: !this.state.openAlgoMenu});
   }
 }
 
