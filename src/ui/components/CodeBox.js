e4c1848aab517a14b380dd65a9da80d3de76c78d
Reminisque <travisc2728@gmail.com>
2020-10-05 02:28:30 -0700

    Add default text and highlight colors for CodeBox

--- src/ui/components/CodeBox.js
+++ src/ui/components/CodeBox.js
@@ -2,13 +2,15 @@ import React from 'react';
 import styles from './CodeBox.module.css';
 
 function CodeBox(props) {
+  const highlightColor = props.highlightColor || 'rgba(184, 163, 163, 0.75)';
+
   return (
     <div className={[styles.root, props.className].join(' ')}>
       {props.code.map((code, lineNum) =>
         <div key={lineNum} className={styles.line}>
-        <div className={styles.lineNum}>{lineNum + 1}</div>
-          {props.highlightSet?.has(lineNum + 1) 
-            ? <code className={styles.code} style={{background: props.highlightColor}}>{code}</code>
+          <div className={styles.lineNum}>{lineNum + 1}</div>
+          {props.highlightSet?.has(lineNum + 1)
+            ? <code className={styles.code} style={{ background: highlightColor }}>{code}</code>
             : <code className={styles.code}>{code}</code>
           }
         </div>
