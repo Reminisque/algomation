ae94139a11abf4beb487ce719fe3455b159d55d4
Reminisque <travisc2728@gmail.com>
2020-11-08 22:12:22 -0800

    Updates

---
+++ precache-manifest.253f1c284b4e4ee1c0d87e97ac57cca5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8392c7e0962547f8065f9635b284938c",
+    "url": "/algomation/index.html"
+  },
+  {
+    "revision": "f0541a7e879d94e42928",
+    "url": "/algomation/static/css/main.af60333a.chunk.css"
+  },
+  {
+    "revision": "5904fa7127c40c14bdb7",
+    "url": "/algomation/static/js/2.4ab06db3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/algomation/static/js/2.4ab06db3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0541a7e879d94e42928",
+    "url": "/algomation/static/js/main.a7fe9fe0.chunk.js"
+  },
+  {
+    "revision": "4cdd25f8d9cf8844da40",
+    "url": "/algomation/static/js/runtime-main.b18552b2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/algomation/static/media/logo.5d5d9eef.svg"
+  }
+]);
